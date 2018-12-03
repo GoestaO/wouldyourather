c113adc22207ec165a859d08540f33f1794cb349
Gösta Ostendorf <gostendorf@zalando.de>
2018-12-03 13:38:52 +0100

    Updates

---
+++ precache-manifest.5309d2444bdbd38241cff07e0448bcbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f85d719844c151c3a94",
+    "url": "/github.io/wouldyourather/static/js/runtime~main.0f85d719.js"
+  },
+  {
+    "revision": "a736a67cc0e1c31882f3",
+    "url": "/github.io/wouldyourather/static/js/main.a736a67c.chunk.js"
+  },
+  {
+    "revision": "c9fbdb625f1cc7d62be2",
+    "url": "/github.io/wouldyourather/static/js/1.c9fbdb62.chunk.js"
+  },
+  {
+    "revision": "a736a67cc0e1c31882f3",
+    "url": "/github.io/wouldyourather/static/css/main.506c9836.chunk.css"
+  },
+  {
+    "revision": "c9fbdb625f1cc7d62be2",
+    "url": "/github.io/wouldyourather/static/css/1.865d4d51.chunk.css"
+  },
+  {
+    "revision": "698364387f15c57e0aea0526762dc7ed",
+    "url": "/github.io/wouldyourather/index.html"
+  }
+];
